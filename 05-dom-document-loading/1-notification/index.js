a700e2b30ef523a30a60adea89805a7662484343
TimofeevAnton1980 <timofeevai1980@gmail.com>
2024-11-27 20:27:09 +0300

    задача 5_1

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,50 @@
 export default class NotificationMessage {
-
+  constructor(text = "", parameters = {}) {
+    const { duration = 2000, type = "success" } = parameters;
+    this.text = text;
+    this.duration = duration;
+    this.type = type;
+    this.element = this.createElement(this.notificationElement());
+  }
+  static value = null;
+  createElement(html) {
+    const div = document.createElement("div");
+    div.innerHTML = html;
+    return div.firstElementChild;
+  }
+  notificationElement() {
+    return `
+    <div class="notification ${this.type}" style="--value:${this.duration}s">
+      <div class="timer"></div>
+      <div class="inner-wrapper">
+        <div class="notification-header">${this.type}</div>
+        <div class="notification-body">${this.text}</div>
+      </div>
+    </div>
+    `;
+  }
+  show(target = document.body) {
+    if (NotificationMessage.value === null) {
+      NotificationMessage.value = this.element;
+      target.appendChild(this.element);
+      this.timer();
+    } else {
+      NotificationMessage.value.remove();
+      target.appendChild(this.element);
+      NotificationMessage.value = this.element;
+      this.timer();
+    }
+  }
+  remove() {
+    this.element.remove();
+  }
+  destroy() {
+    this.remove();
+    NotificationMessage.value = null;
+  }
+  timer() {
+    setTimeout(() => {
+      this.destroy();
+    }, this.duration);
+  }
 }
